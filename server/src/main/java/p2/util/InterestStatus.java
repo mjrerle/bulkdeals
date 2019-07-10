4cd2ee89fdb6c5fc916688119836a1c042682a7f
Vajira Hapu Arachchige <51335173+vajirakarunathilake@users.noreply.github.com>
2019-07-09 20:43:08 -0400

    DAO Folder

--- server/src/main/java/p2/util/InterestStatus.java
+++ server/src/main/java/p2/util/InterestStatus.java
@@ -3,8 +3,8 @@ package p2.util;
 public enum InterestStatus {
 	
 	PENDING(0),
-	NOR_ENOUGH_INTEREST(1),
-	CANCELED_BY_SELLER(2),
+	NOT_ENOUGH_INTEREST(1),
+	CANCELLED_BY_SELLER(2),
 	ACCEPTED(3);
 	
 	public final int value;
