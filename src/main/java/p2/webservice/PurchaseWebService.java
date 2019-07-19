48609322b8f4d78d2649dac6fe4fc80eddca3926
Vajira Hapu Arachchige <51335173+vajirakarunathilake@users.noreply.github.com>
2019-07-19 19:29:16 -0400

    Purchase inventory management updated.

--- src/main/java/p2/webservice/PurchaseWebService.java
+++ src/main/java/p2/webservice/PurchaseWebService.java
@@ -20,173 +20,182 @@ import p2.service.ProductService;
 import p2.service.PurchaseService;
 import p2.service.UserService;
 import p2.util.Glogger;
+import p2.util.ThresholdStatus;
 import p2.util.ValidationUtilities;
 
 public class PurchaseWebService {
-  public static Logger logger = Glogger.logger;
+	public static Logger logger = Glogger.logger;
 
-  public static void insert(HttpServletRequest request, HttpServletResponse response) {
+	public static void insert(HttpServletRequest request, HttpServletResponse response) {
 
-    ObjectMapper mapper = new ObjectMapper();
-    Purchase purchase = null;
-    try {
-      purchase = mapper.readValue(request.getInputStream(), Purchase.class);
-    } catch (JsonParseException e1) {
-      e1.printStackTrace();
-    } catch (JsonMappingException e1) {
-      e1.printStackTrace();
-    } catch (IOException e1) {
-      e1.printStackTrace();
-    }
-    int purchaseId = -1;
-    if (purchase != null) {
-      // check if purchase has a product and a user
-      if (purchase.getProduct() != null && purchase.getUser() != null) {
-        Product product = ProductService.findById(purchase.getProduct().getProductId());
-        User user = UserService.findById(purchase.getUser().getUserId());
-        // if it does, then make sure that the user and the tax exist in the db
-        if (product != null && user != null) {
-          LocalDate now = LocalDate.now();
-          purchase.setDatePurchased(now);
-          purchaseId = PurchaseService.insert(purchase);
-        }
-      }
-    }
-    try {
-      response.setContentType("text/html");
-      response.setCharacterEncoding("UTF-8");
-      if (purchaseId >= 0) {
-        response.getWriter().append("true").close();
-      } else {
-        response.getWriter().append("false").close();
-      }
+		ObjectMapper mapper = new ObjectMapper();
+		Purchase purchase = null;
+		int purchaseQuantity = 0;
+		try {
+			purchase = mapper.readValue(request.getInputStream(), Purchase.class);
+		} catch (JsonParseException e1) {
+			e1.printStackTrace();
+		} catch (JsonMappingException e1) {
+			e1.printStackTrace();
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+		int purchaseId = -1;
+		if (purchase != null) {
+			// check if purchase has a product and a user
+			if (purchase.getProduct() != null && purchase.getUser() != null) {
+				Product product = ProductService.findById(purchase.getProduct().getProductId());
+				User user = UserService.findById(purchase.getUser().getUserId());
+				// if it does, then make sure that the user and the tax exist in the db
+				if (product != null && user != null) {
 
-    } catch (IOException e) {
-      logger.warn(e.getMessage());
-      e.printStackTrace();
-    }
-  }
+					if (product.getStatus().equals(ThresholdStatus.PRETTY.value)) {
+						purchaseQuantity = (int) (purchase.getCost() / purchase.getCost());
+						product.setInterestThreshold(product.getInterestThreshold() - purchaseQuantity);
+					}
 
-  public static void update(HttpServletRequest request, HttpServletResponse response) {
+					purchase.setProduct(product);
+					purchase.setUser(user);
+					purchase.setDatePurchased(LocalDate.now());
+					purchaseId = PurchaseService.insert(purchase);
+				}
+			}
+		}
+		try {
+			response.setContentType("text/html");
+			response.setCharacterEncoding("UTF-8");
+			if (purchaseId >= 0) {
+				response.getWriter().append("true").close();
+			} else {
+				response.getWriter().append("false").close();
+			}
 
-    ObjectMapper mapper = new ObjectMapper();
-    Purchase purchase = null;
-    try {
-      purchase = mapper.readValue(request.getInputStream(), Purchase.class);
-    } catch (JsonParseException e1) {
-      e1.printStackTrace();
-    } catch (JsonMappingException e1) {
-      e1.printStackTrace();
-    } catch (IOException e1) {
-      e1.printStackTrace();
-    }
-    boolean success = false;
-    if (purchase != null) {
-      if (purchase.getPurchaseId() >= 0) {
-        // check if favorite has a product and a user
-        if (purchase.getProduct() != null && purchase.getUser() != null) {
-          Product product = ProductService.findById(purchase.getProduct().getProductId());
-          User user = UserService.findById(purchase.getUser().getUserId());
-          // if it does, then make sure that the user and the tax exist in the db
-          if (product != null && user != null) {
-            success = PurchaseService.update(purchase);
-          }
-        }
-      }
-    }
+		} catch (IOException e) {
+			logger.warn(e.getMessage());
+			e.printStackTrace();
+		}
+	}
 
-    try {
-      response.setContentType("text/html");
-      response.setCharacterEncoding("UTF-8");
-      if (success) {
-        response.getWriter().append("Purchase Updated").close();
-      } else {
-        response.getWriter().append("Purchase Update Failed").close();
-      }
-    } catch (IOException e) {
-      logger.warn(e.getMessage());
-      e.printStackTrace();
-    }
-  }
+	public static void update(HttpServletRequest request, HttpServletResponse response) {
 
-  public static void deleteById(HttpServletRequest request, HttpServletResponse response) {
-    ObjectMapper mapper = new ObjectMapper();
-    Purchase purchase = null;
-    try {
-      purchase = mapper.readValue(request.getInputStream(), Purchase.class);
-    } catch (JsonParseException e1) {
-      e1.printStackTrace();
-    } catch (JsonMappingException e1) {
-      e1.printStackTrace();
-    } catch (IOException e1) {
-      e1.printStackTrace();
-    }
-    boolean success = false;
-    if (purchase != null) {
-      if (purchase.getPurchaseId() >= 0) {
-        // check if favorite has a product and a user
-        if (purchase.getProduct() != null && purchase.getUser() != null) {
-          Product product = ProductService.findById(purchase.getProduct().getProductId());
-          User user = UserService.findById(purchase.getUser().getUserId());
-          // if it does, then make sure that the user and the tax exist in the db
-          if (product != null && user != null) {
-            success = PurchaseService.deleteById(purchase.getPurchaseId());
-          }
-        }
-      }
-    }
+		ObjectMapper mapper = new ObjectMapper();
+		Purchase purchase = null;
+		try {
+			purchase = mapper.readValue(request.getInputStream(), Purchase.class);
+		} catch (JsonParseException e1) {
+			e1.printStackTrace();
+		} catch (JsonMappingException e1) {
+			e1.printStackTrace();
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+		boolean success = false;
+		if (purchase != null) {
+			if (purchase.getPurchaseId() >= 0) {
+				// check if favorite has a product and a user
+				if (purchase.getProduct() != null && purchase.getUser() != null) {
+					Product product = ProductService.findById(purchase.getProduct().getProductId());
+					User user = UserService.findById(purchase.getUser().getUserId());
+					// if it does, then make sure that the user and the tax exist in the db
+					if (product != null && user != null) {
+						success = PurchaseService.update(purchase);
+					}
+				}
+			}
+		}
 
-    try {
-      response.setContentType("text/html");
-      response.setCharacterEncoding("UTF-8");
-      if (success) {
-        response.getWriter().append("Purchase Deleted").close();
-      } else {
-        response.getWriter().append("Purchase Delete Failed").close();
-      }
-    } catch (IOException e) {
-      logger.warn(e.getMessage());
-      e.printStackTrace();
-    }
-  }
+		try {
+			response.setContentType("text/html");
+			response.setCharacterEncoding("UTF-8");
+			if (success) {
+				response.getWriter().append("Purchase Updated").close();
+			} else {
+				response.getWriter().append("Purchase Update Failed").close();
+			}
+		} catch (IOException e) {
+			logger.warn(e.getMessage());
+			e.printStackTrace();
+		}
+	}
 
-  public static void findAll(HttpServletRequest request, HttpServletResponse response) {
+	public static void deleteById(HttpServletRequest request, HttpServletResponse response) {
+		ObjectMapper mapper = new ObjectMapper();
+		Purchase purchase = null;
+		try {
+			purchase = mapper.readValue(request.getInputStream(), Purchase.class);
+		} catch (JsonParseException e1) {
+			e1.printStackTrace();
+		} catch (JsonMappingException e1) {
+			e1.printStackTrace();
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+		boolean success = false;
+		if (purchase != null) {
+			if (purchase.getPurchaseId() >= 0) {
+				// check if favorite has a product and a user
+				if (purchase.getProduct() != null && purchase.getUser() != null) {
+					Product product = ProductService.findById(purchase.getProduct().getProductId());
+					User user = UserService.findById(purchase.getUser().getUserId());
+					// if it does, then make sure that the user and the tax exist in the db
+					if (product != null && user != null) {
+						success = PurchaseService.deleteById(purchase.getPurchaseId());
+					}
+				}
+			}
+		}
 
-    List<Purchase> purchases = PurchaseService.findAll();
+		try {
+			response.setContentType("text/html");
+			response.setCharacterEncoding("UTF-8");
+			if (success) {
+				response.getWriter().append("Purchase Deleted").close();
+			} else {
+				response.getWriter().append("Purchase Delete Failed").close();
+			}
+		} catch (IOException e) {
+			logger.warn(e.getMessage());
+			e.printStackTrace();
+		}
+	}
 
-    try {
-      ObjectMapper om = new ObjectMapper();
-      String json = om.writeValueAsString(purchases);
-      response.setContentType("application/json");
-      response.setCharacterEncoding("UTF-8");
-      response.getWriter().append(json).close();
-    } catch (IOException e) {
-      logger.warn(e.getMessage());
-      e.printStackTrace();
-    }
-  }
+	public static void findAll(HttpServletRequest request, HttpServletResponse response) {
 
-  public static void findById(HttpServletRequest request, HttpServletResponse response) {
+		List<Purchase> purchases = PurchaseService.findAll();
 
-    int purchaseId = -1;
-    String maybePurchaseId = request.getParameter("purchaseId");
-    Purchase purchase = null;
+		try {
+			ObjectMapper om = new ObjectMapper();
+			String json = om.writeValueAsString(purchases);
+			response.setContentType("application/json");
+			response.setCharacterEncoding("UTF-8");
+			response.getWriter().append(json).close();
+		} catch (IOException e) {
+			logger.warn(e.getMessage());
+			e.printStackTrace();
+		}
+	}
 
-    if (ValidationUtilities.checkNullOrEmpty(maybePurchaseId)) {
-      purchaseId = Integer.parseInt(maybePurchaseId);
-      purchase = PurchaseService.findById(purchaseId);
-    }
+	public static void findById(HttpServletRequest request, HttpServletResponse response) {
 
-    try {
-      ObjectMapper om = new ObjectMapper();
-      String json = om.writeValueAsString(purchase);
-      response.setContentType("application/json");
-      response.setCharacterEncoding("UTF-8");
-      response.getWriter().append(json).close();
-    } catch (IOException e) {
-      logger.warn(e.getMessage());
-      e.printStackTrace();
-    }
-  }
+		int purchaseId = -1;
+		String maybePurchaseId = request.getParameter("purchaseId");
+		Purchase purchase = null;
+
+		if (ValidationUtilities.checkNullOrEmpty(maybePurchaseId)) {
+			purchaseId = Integer.parseInt(maybePurchaseId);
+			purchase = PurchaseService.findById(purchaseId);
+		}
+
+		try {
+			ObjectMapper om = new ObjectMapper();
+			String json = om.writeValueAsString(purchase);
+			response.setContentType("application/json");
+			response.setCharacterEncoding("UTF-8");
+			response.getWriter().append(json).close();
+		} catch (IOException e) {
+			logger.warn(e.getMessage());
+			e.printStackTrace();
+		}
+	}
 
 }
